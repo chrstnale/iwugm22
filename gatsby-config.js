caa602952812b574d34e9c724ce2da7dab54443f
chrstnale <christianale85@gmail.com>
2022-04-17 20:28:36 +0700

    fix: GA4 config with gtag plugin and firebase build command

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,19 @@
 module.exports = {
   siteMetadata: {
     title: `IW UGM 2022`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.iwugm2022.web.app`,
   },
   plugins: [
     {
-      trackingId: "G-LHB61EC407",
-    }
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "G-LHB61EC407", 
+        ],
+        pluginConfig: {
+          head: true,
+        },
+      },
+    },
   ],
 }
