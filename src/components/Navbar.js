f15339c386f61284d4287da1ae69dac737eaa14a
chrstnale <christianale85@gmail.com>
2022-05-16 12:17:06 +0700

    fix(navbar, home): fix some layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,40 @@
 import { StaticImage } from 'gatsby-plugin-image';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {Link} from 'react-scroll'
 import { FaTimes, FaBars } from 'react-icons/fa';
 
 export default function Navbar(){
+    // is navbar button hamburger or times?
     const [isClicked, setIsClicked] = useState(false); 
+    // is navbar items displaying? (on mobile)
     const [itemDisplay, setItemDisplay] = useState(false);
+    // is navbar has shadow?
+    const [navbarShadow, setNavbarShadow] = useState(false)
     const handleClick = () => {
         setIsClicked(!isClicked)
         setItemDisplay(!itemDisplay)
     }
 
+    const changeShadow = () => {
+        if (window.scrollY >= 100) {
+          setNavbarShadow(true)
+        } else {
+          setNavbarShadow(false)
+        }
+      }
+    
+      useEffect(() => {
+        changeShadow()
+        // adding the event when scroll change background
+        window.addEventListener("scroll", changeShadow)
+      }, (window.scrollY))
+
     return(
-        <header className="fixed top-0 left-0 w-screen p-3 h-16 z-10 whitespace-nowrap
-                            flex justify-between bg-white shadow-lg items-center">
+        <header className={`fixed top-0 left-0 w-screen p-0 md:p-3 h-9 md:h-16 z-10 whitespace-nowrap
+                            flex justify-between items-center ${navbarShadow ? 'shadow-lg bg-white' : ''}
+                            transition-all duration-300 ease-in-out`}>
             <div className="flex items-center">
-                <Link activeClass="active" to="home" spy={true} smooth={true} offset={-64} className='navbar-items flex items-center text-lg font-semibold rounded-md px-3'>
+                <Link activeClass="active" to="home" spy={true} smooth={true} offset={-64} className='navbar-items flex items-center text-base md:text-lg font-semibold rounded-md px-3 py-0'>
                     <StaticImage 
                         src='../images/iwugm.png'
                         alt="logo iwugm22"
@@ -26,11 +45,11 @@ export default function Navbar(){
                     IW UGM
                 </Link>
             </div>
-            <div className='p-1 px-4 ease-in-out duration-1000 mx-3 rounded-lg font-light text-xs md:text-sm hover:text-blue md:hidden'>
-                {isClicked ? <FaTimes onClick={handleClick} className='text-blue cursor-pointer'/> : <FaBars onClick={handleClick} className='cursor-pointer'/>}
+            <div className='p-1 px-4 mx-3 rounded-lg font-light text-xs md:text-sm hover:text-blue md:hidden'>
+                {isClicked ? <FaTimes onClick={handleClick} className='text-blue cursor-pointer transition-all ease-in-out duration-1000'/> : <FaBars onClick={handleClick} className='cursor-pointer'/>}
             </div>
-            <div className={`${itemDisplay ? 'block' : 'hidden'} md:block items-center w-full md:w-auto flex flex-col absolute left-0 top-16 md:flex-row md:static transition-all duration-200 ease-in-out`}>
-                <Link activeClass="active" to="home" spy={true} smooth={true} offset={-120} className='navbar-items' onClick={handleClick}>
+            <div className={`${itemDisplay ? 'block bg-white' : 'hidden'} md:block items-center w-full md:w-auto flex flex-col absolute left-0 top-9 md:top-16 md:flex-row md:static transition-all duration-500 ease-in-out`}>
+                <Link activeClass="active" to="home" spy={true} smooth={true} offset={-64} className='navbar-items' onClick={handleClick}>
                     Home
                 </Link>
                 <Link activeClass="active" to="about" spy={true} smooth={true} offset={-64} className="navbar-items" onClick={handleClick}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,8 @@ const IndexPage = () => {
           <link rel="icon" href={iwugm} />
       </Helmet>
       <Navbar/>
-      <div className="absolute top-16 overflow-x-hidden bg-white" id="pages">
-        <section id="home" className="h-screen w-full flex flex-col md:flex-row items-center justify-center pl-8 md:pl-3"
+      <div className="absolute overflow-x-hidden bg-white" id="pages">
+        <section id="home" className="min-h-screen w-full flex flex-col md:flex-row items-center justify-center pl-8 md:pl-3"
           // data-sal="slide-up"
           // data-sal-duration="1000"
           // data-sal-easing="ease-in-out"
@@ -29,25 +29,25 @@ const IndexPage = () => {
                 Industrial<br/>Workshop<br/><span className='font-light'>UGM 20<span className='font-semibold'>22</span></span>
             </h1>
           </section>
-          <section id="about" className="h-screen text-center text-white bg-black">
+          <section id="about" className="min-h-screen text-center text-white bg-black">
             <h1>This is About section</h1>
             <p>Industrial Workshop is an annual event held by Himpunan Mahasiswa Teknik Industri Universitas</p>
             <br/>
             <p>
-              This year is the 9ᵗʰ year of Industrial Workshop. The history of this event is started with Ernographic
+              This year is the 10ᵗʰ year of Industrial Workshop. The history of this event is started with Ernographic
               Design Workshop (EDW), which then became an annual workshop that discusing about current situatuion.
               Starting in 2019, Industrial Workshop made a branch event, White Paper Competition, and it is continued
               this year.</p>
           </section>
-          <section id="workshop" className="h-screen">
+          <section id="workshop" className="min-h-screen">
           <h1>This is Workshop section</h1>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus. Totam nihil similique a repellat minus dolor amet quasi. Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.
           </section>
-          <section id="wpc" className="h-screen text-white bg-black">
+          <section id="wpc" className="min-h-screen text-white bg-black">
           <h1>This is WPC section</h1>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus. Totam nihil similique a repellat minus dolor amet quasi. Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.
           </section>
-          <section id="sponsor" className="h-screen">
+          <section id="sponsor" className="">
           <h1>This is Sponsor section</h1>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus. Totam nihil similique a repellat minus dolor amet quasi. Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.
           </section>
