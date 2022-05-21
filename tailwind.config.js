80eea256c65f61b2056c46e1078b91581e4e9372
chrstnale <christianale85@gmail.com>
2022-05-21 07:59:44 +0700

    fix(font)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-      axiforma: ['Axiforma', 'sans-serif'],
+      axiforma: ['Axiforma'],
       body: ['Montserrat'],
       deco: ['Playfair']
     },
